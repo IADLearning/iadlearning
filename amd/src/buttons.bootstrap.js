d18051fe4bf49bfd73bc0819cc7ec2ec51f9ff72
Jose A Omedes <jose.omedes@itoptraining.com>
2018-01-22 18:37:29 +0100

    Add datatables amd modules

---
+++ amd/src/buttons.bootstrap.js
@@ -0,0 +1,30 @@
+define(["jquery", "mod_iadlearning/jquery.dataTables"], function(jQuery) {
+  (function($, DataTables) {
+    $.extend(true, DataTables.Buttons.defaults, {
+      dom: {
+        container: {
+          className: "dt-buttons btn-group"
+        },
+        button: {
+          className: "btn btn-default"
+        },
+        collection: {
+          tag: "ul",
+          className: "dt-button-collection dropdown-menu",
+          button: {
+            tag: "li",
+            className: "dt-button"
+          },
+          buttonLiner: {
+            tag: "a",
+            className: ""
+          }
+        }
+      }
+    });
+
+    DataTables.ext.buttons.collection.text = function(dt) {
+      return dt.i18n("buttons.collection", 'Collection <span class="caret"/>');
+    };
+  })(jQuery, jQuery.fn.dataTable);
+});
